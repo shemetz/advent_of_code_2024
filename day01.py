e3dd168e5627168142da857c4397f938f33467c8
itamar <itamarcu@gmail.com>
2024-12-03 20:13:42 +0200

    Initial commit

---
+++ day01.py
@@ -0,0 +1,21 @@
+with open("input.txt") as input_file:
+    input_lines = input_file.readlines()
+    input_lines = [line.strip('\n') for line in input_lines]
+
+
+print(11111)  #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(22222)  #
