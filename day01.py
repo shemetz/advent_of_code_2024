8bf1adac358d0fcd2aa578d076cd96e7836416c5
itamar <itamarcu@gmail.com>
2024-12-03 20:39:53 +0200

    Day 1

--- day01.py
+++ day01.py
@@ -2,20 +2,19 @@ with open("input.txt") as input_file:
     input_lines = input_file.readlines()
     input_lines = [line.strip('\n') for line in input_lines]
 
+left, right = [], []
+for line in input_lines:
+    l, *_, r = line.split(" ")
+    left.append(int(l))
+    right.append(int(r))
 
-print(11111)  #
+answer_1 = sum(
+    abs(l - r) for l, r in zip(sorted(left), sorted(right))
+)
+print(answer_1)  # 1222801
 
+answer_2 = sum(
+    l * sum(1 if l == r else 0 for r in right) for l in left
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-print(22222)  #
+print(answer_2)  # 22545250
